d7770db0f8fe05c6d683ccc093a5419143c04808
Yashika-P <yyashika118@gmail.com>
2024-10-08 11:03:30 +0530

    init commit

---
+++ Quires/Queries.sql
@@ -0,0 +1,16 @@
+-- CREATE DATABASE ecommerce
+-- use ecommerce
+-- CREATE TABLE customer
+-- (id INT(4) NOT NULL auto_increment,
+ -- customername VARCHAR(20) NOT NULL,
+ -- email VARCHAR(40) NOT NULL, 
+ -- address VARCHAR(50) NOT NULL,
+ -- PRIMARY KEY(`id`)
+-- ) ENGINE = InnoDB;
+-- INSERT INTO customer(id,customername,email,address) VALUES(NULL,"nivetha","nivetha@123","no.13 kamarjar street");
+-- INSERT INTO customer(id,customername,email,address) VALUES(NULL,"raghul","raghul@324","no.9 gopal street");
+-- INSERT INTO customer(id,customername,email,address) VALUES(NULL,"kalaiyarasi","kalai@09","no:10 raja street");
+-- INSERT INTO customer(id,customername,email,address) VALUES(NULL,"alfert regan","alfertragan@98","no:5 ksp mahal");
+-- INSERT INTO customer(id,customername,email,address) VALUES(NULL,"harini","harini@098","no:11 thiru nagar");
+-- INSERT INTO  customer(id,customername,email,address) VALUES(NULL,"nalin","nalinyashi@109","no:34 mogappair");
+
